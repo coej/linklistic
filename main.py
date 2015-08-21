1f4f4734f0e241449945efca0e01dc57b0c20743
Chris Jenkins <chrisoej@gmail.com>
2015-08-21 12:50:08 -0400

    partially setting up template title/name/message

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 
-from flask import Flask, request, render_template, send_from_directory
+from flask import Flask, request, render_template
 
 
 app = Flask(__name__)
@@ -12,31 +12,17 @@ class Link:
         self.href = path
         self.title = title
 
+# Consider:
+# - we can include a self-referential link (localhost:8888/anotherlink.com)
+# - we can title the links (maybe)?
+# - So: if the format is set up the right way it could be possible to encode 
+#   an entire choose-your-own-adventure game into one big URL string...?
 
 # "Catch-all URL": http://flask.pocoo.org/snippets/57/
 @app.route('/', defaults={'submitted_text': ''})
 @app.route('/<path:submitted_text>')
 def catch_all(submitted_text):
-
-    #if submitted_text.lower()[:8] == 'st_test/':
-    #    return './static/' + submitted_text[8:]
-
-    #if submitted_text.lower()[:7] == 'static/':
-    #    static_path = submitted_text[7:]
-    #    print('fff')
-    #    if static_path in ['css/bootstrap.min.css',
-    #                       'css/grid.css',
-    #                       'css/starter-template.css',
-    #                       'img/favicon.ico',
-    #                       'js/bootstrap.min.js',
-    #                       'js/ie10-viewport-bug-workaround.js',
-    #                       'js/jquery.min.js']:
-    #    #    return send_from_directory('./static', static_path)
-    #    #elif not static_path:
-    #    #    return "blank path"
-    #    #else:
-    #        return "static path not found: " % static_path
-
+    # '/static/' urls are sent to file system by default
 
     if submitted_text.lower() in ['', 'index.html', 'index.htm']:
         return "Front page here"
@@ -49,22 +35,40 @@ def catch_all(submitted_text):
 
     else:
         links = []
+        params = {'from': None,
+                  'message': None,
+                  'title': "Your links are listed below."}
 
+        # users must use two semicolons for each semicolon that's in an actual URL to be linked
         strings = submitted_text.replace(';;', 'ESCAPED_SEMICOLON_STANDIN')  
         #we'll need more general-purpose escaping to avoid HTML issues with angle brackets etc.
 
         strings = strings.split(';')
         for s in strings:
+
+            # functionality for adding commands into the 
+            # link list string ("listname:My Bookmarks;http://...")
+            
+            check_commands = s.split('=', maxsplit=1)
+            if len(check_commands)==2:
+                key = check_commands[0]
+                value = check_commands[1]
+                params[key] = value
+
             s = s.replace('ESCAPED_SEMICOLON_STANDIN', ';')
 
-            lnk = Link(s, 'some title')
+            title = s
+            if len(title) > 77:
+                title = title[:77] + '...'
+
+            lnk = Link(path=s, title=title)
             links.append(lnk)
 
-        # send strings list to jinja template
-        return render_template('main.html', links=links)
-        #return str(
-        #    '<br>'.join(build_output_temp)
-        #)
+        # send Link list to jinja template
+        return render_template('main.html', links=links, 
+                                            list_from=params['from'],
+                                            message=params['message'],
+                                            title=params['title'])
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8888, debug=True)
