d8559c9bc0c22c7eabcf1ef9c8fd411d99c3c002
Chris Jenkins <chrisoej@gmail.com>
2015-08-21 15:38:21 -0400

    debug output setup

--- main.py
+++ main.py
@@ -2,9 +2,14 @@
 from __future__ import print_function
 from flask import Flask, request, render_template
 
+# rendered below closing HTML tag!
+debug_output = []
+
 # heroku postgres boilerplate
 # the environment variable DATABASE_URL has to be set manually on Heroku 
 # (e.g., on the app settings page)
+# set with: 
+# confirm with: heroku config:get DATABASE_URL --app linklistic
 import os
 import psycopg2
 import urlparse
@@ -17,7 +22,8 @@ conn = psycopg2.connect(
     host=url.hostname,
     port=url.port
 )
-debug_output = str(conn)
+debug.append(str(conn))
+
 
 
 default_params = {'by': None,
@@ -60,44 +66,44 @@ def catch_all(submitted_text):
     if submitted_text.lower() in ['usage', 'usage/', 'usage.html']:
         return "Usage page here"
 
-    else:
-        links = []
-        params = default_params.copy()
+    links = []
+    params = default_params.copy()
 
-        # users must use two semicolons for each semicolon that's in an actual URL to be linked
-        strings = submitted_text.replace(';;', 'ESCAPED_SEMICOLON_STANDIN')  
-        #we'll need more general-purpose escaping to avoid HTML issues with angle brackets etc.
+    # users must use two semicolons for each semicolon that's in an actual URL to be linked
+    strings = submitted_text.replace(';;', 'ESCAPED_SEMICOLON_STANDIN')  
+    #we'll need more general-purpose escaping to avoid HTML issues with angle brackets etc.
 
-        strings = strings.split(';')
-        for s in strings:
+    strings = strings.split(';')
+    for s in strings:
 
-            # functionality for adding commands into the 
-            # link list string ("listname:My Bookmarks;http://...")
-            
-            check_commands = s.split('=')  #, maxsplit=1) py 3 only
-            if len(check_commands)>=2:
-                key = check_commands[0]
-                value = '='.join(check_commands[1:])
-                params[key] = value
-                continue
+        # functionality for adding commands into the 
+        # link list string ("listname:My Bookmarks;http://...")
+        
+        check_commands = s.split('=')  #, maxsplit=1) py 3 only
+        if len(check_commands)>=2:
+            key = check_commands[0]
+            value = '='.join(check_commands[1:])
+            params[key] = value
+            continue
 
-            s = s.replace('ESCAPED_SEMICOLON_STANDIN', ';')
+        s = s.replace('ESCAPED_SEMICOLON_STANDIN', ';')
 
-            title = s
-            if len(title) > 77:
-                title = title[:77] + '...'
+        title = s
+        if len(title) > 77:
+            title = title[:77] + '...'
 
-            lnk = Link(path=s, title=title)
-            links.append(lnk)
+        lnk = Link(path=s, title=title)
+        links.append(lnk)
 
-        # send Link list to jinja template
+    # send Link list to jinja template
 
-        return render_template('main.html', links=links, 
-                                            by=params['by'],
-                                            message=params['message'],
-                                            title=params['title'],
-                                            brand_message = params['brand_message'],
-                                            full_query = submitted_text)             + debug_output
+    return render_template('main.html', links=links, 
+                                        by=params['by'],
+                                        message=params['message'],
+                                        title=params['title'],
+                                        brand_message = params['brand_message'],
+                                        full_query = submitted_text
+                            ) + '\n\n' + '\n\n'.join(debug_output)
 
 if __name__ == '__main__':
     import os
